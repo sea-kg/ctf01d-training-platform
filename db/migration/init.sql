e1c03898d50430611482fdbc3ae77cd2cfeaef97
IgorPolyakov <igorpolyakov@protonmail.com>
2024-04-19 10:36:18 +0300

    migrate mysql to psql schema

--- db/migration/init.sql
+++ db/migration/init.sql
@@ -1,65 +1,45 @@
 -- Пользователи
-CREATE TABLE `users` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `user_name` varchar(255) NOT NULL,
-    `password_hash` varchar(255) NOT NULL,
-    `avatar_url` varchar(255),
-    `role` enum('admin', 'player') NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(255),
+    role VARCHAR(255) NOT NULL CHECK (role IN ('admin', 'player'))
 );
 
 -- Команды
-CREATE TABLE `teams` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `team_name` varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `university` varchar(255),
-    `social_links` text,
-    `avatar_url` varchar(255),
-    PRIMARY KEY (`id`)
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    team_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    university VARCHAR(255),
+    social_links TEXT,
+    avatar_url VARCHAR(255)
 );
 
 -- Сервисы
-CREATE TABLE `services` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `author` varchar(255) NOT NULL,
-    `logo_url` varchar(255),
-    `description` text,
-    `is_public` boolean NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE services (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    logo_url VARCHAR(255),
+    description TEXT,
+    is_public BOOLEAN NOT NULL
 );
 
 -- Игры
-CREATE TABLE `games` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `start_time` DATETIME NOT NULL,
-    `end_time` DATETIME NOT NULL,
-    `description` text,
-    PRIMARY KEY (`id`)
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    description TEXT
 );
 
--- Флаги
-CREATE TABLE `flags` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `external_id` varchar(255) NOT NULL,
-    `flag` varchar(255) NOT NULL,
-    `service_id` int,
-    `game_id` int,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`service_id`) REFERENCES `services` (`id`),
-    FOREIGN KEY (`game_id`) REFERENCES `games` (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-
 -- Результаты игр
-CREATE TABLE `results` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `team_id` int,
-    `game_id` int,
-    `score` int NOT NULL,
-    `rank` int NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`),
-    FOREIGN KEY (`game_id`) REFERENCES `games` (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE results (
+    id SERIAL PRIMARY KEY,
+    team_id INTEGER REFERENCES teams(id),
+    game_id INTEGER REFERENCES games(id),
+    score INTEGER NOT NULL,
+    rank INTEGER NOT NULL
+);
