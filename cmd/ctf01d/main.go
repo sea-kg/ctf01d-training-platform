4e7ee5d0221746d4a4f015fb67bce61545e1de59
Evgenii Sopov <mrseakg@gmail.com>
2024-06-06 18:05:54 +0700

    Redesign database updates struct

--- cmd/ctf01d/main.go
+++ cmd/ctf01d/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	config "ctf01d/configs"
 	"ctf01d/internal/app/routers"
+	models "ctf01d/internal/app/db"
 	"database/sql"
 	"log/slog"
 	"net/http"
@@ -15,6 +16,8 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
+	slog.Error("Starting server...")
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		slog.Error("Config error: " + err.Error())
@@ -22,6 +25,14 @@ func main() {
 	db, err := sql.Open(cfg.DB.Driver, cfg.DB.DataSource)
 	if err != nil {
 		slog.Error("Error connecting to the database: " + err.Error())
+	} else {
+		query := `SELECT update_id FROM database_updates WHERE id=(SELECT max(id) FROM database_updates)`
+		last_update := &models.DatabaseUpdate{}
+		err := db.QueryRow(query).Scan(&last_update.Id, &last_update.StartTime, &last_update.UpdateId, &last_update.Description)
+		if err != nil {
+			slog.Error("Problem with database: " + err.Error())
+			return
+		}
 	}
 	defer db.Close()
 	router := routers.NewRouter(db)
